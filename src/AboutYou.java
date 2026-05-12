1993bee62c0ac19fef5190ca431856a6b99700d2
Aman Berhe <S-Aman.Berhe@lwtech.edu>
2026-05-12 02:01:49 -0700

    Added imports

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -1,3 +1,11 @@
+import java.awt.Component;
+import java.util.Scanner;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class AboutYou {
+    //Your example code
+
     
 }
